cb74375aab6de74c03b6dfa2593c823d602cc80a
vista-juan-constantine <juan.constantine@vistadash.com>
2024-07-13 13:22:10 -0500

    Added nextui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Chip } from "@nextui-org/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h2>Hola</h2>;
 }
 
-export default App
+export default App;
